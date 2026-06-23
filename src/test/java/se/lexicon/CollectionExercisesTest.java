87a95de309be2498190c0b232db95eea95b4b40c
FSopelsa <felix.sopelsa@gmail.com>
2026-06-23 16:31:47 +0200

    Added tests for E 11-17 ✔️ - 'Tests run: 15, Failures: 0, Errors: 0, Skipped: 0'

--- src/test/java/se/lexicon/CollectionExercisesTest.java
+++ src/test/java/se/lexicon/CollectionExercisesTest.java
@@ -45,4 +45,39 @@ class CollectionExercisesTest {
     void exercise10ShouldRunWithoutCrashing() {
         assertDoesNotThrow(CollectionExercises::exercise10);
     }
+
+    @Test
+    void exercise11ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise11);
+    }
+
+    @Test
+    void exercise12ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise12);
+    }
+
+    @Test
+    void exercise13ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise13);
+    }
+
+    @Test
+    void exercise14ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise14);
+    }
+
+    @Test
+    void exercise15ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise15);
+    }
+
+    @Test
+    void exercise16ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise16);
+    }
+
+    @Test
+    void exercise17ShouldRunWithoutCrashing() {
+        assertDoesNotThrow(CollectionExercises::exercise17);
+    }
 }
